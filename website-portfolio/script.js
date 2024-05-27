502d056a34c82b54fccf512337b98c976c66a2b2
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2024-05-27 15:21:27 +0800

    align image and text of feature page

--- website-portfolio/script.js
+++ website-portfolio/script.js
@@ -63,4 +63,9 @@ window.onload = function () {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
 };
