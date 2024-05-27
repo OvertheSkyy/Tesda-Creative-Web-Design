4509392692afeed74f905a0806f510b5596789ed
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2024-05-27 08:49:20 +0800

    add contents to website portfolio

---
+++ website-portfolio/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instance;
+    // Add click event listener to FAB
+    elems[0].addEventListener('click', function () {
+        console.log('Floating action button clicked');
+    });
+
+    if (elems.length > 0) {
+        instance = M.FloatingActionButton.init(elems, {
+            direction: 'top',
+        });
+    }
+});
