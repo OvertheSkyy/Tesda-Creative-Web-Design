0c37f224b5ce896f86b81ca49f8cb756726586a8
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2024-05-30 01:55:08 +0800

    changed fab js to jquery instance

--- website-portfolio/script.js
+++ website-portfolio/script.js
@@ -1,16 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instance;
-    // Add click event listener to FAB
-    elems[0].addEventListener('click', function () {
-        console.log('Floating action button clicked');
-    });
-
-    if (elems.length > 0) {
-        instance = M.FloatingActionButton.init(elems, {
-            direction: 'top',
-        });
-    }
+// Floating action button
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
 });
 
 // Sidenav view
